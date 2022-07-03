ad577958466009d5fcc6e98c4652664317668b4a
Eduard Tokarev <etdevios@gmail.com>
2022-07-03 12:15:47 +0300

    Completed

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -13,8 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,14 +22,14 @@ class ViewController: UIViewController {
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
         
-        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        // у массивов есть свойство рандомных элементов (индексев):
+        diceImageView1.image = diceArray.randomElement()
         
-        leftDiceNumber = leftDiceNumber + 1
+        // диапазон рандомных индексев можно записать так:
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
         
-        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
-        
-        rightDiceNumber = rightDiceNumber - 1
     }
     
 
