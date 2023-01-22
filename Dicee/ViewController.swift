1ef5fba3c3c808828d9cca8f7c36d78567f23353
Eduard Tokarev <etdevios@gmail.com>
2023-01-22 09:41:34 +0300

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Eduard Tokarev on 22.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
