56d8163a57d25b5a4cdceabe84dc0cf4e7b11534
Eduard Tokarev <etdevios@gmail.com>
2023-01-27 15:43:11 +0300

    41. Storing Data using Variables and Arrays

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -7,21 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var diceImageView1: UIImageView!
-    @IBOutlet weak var diceImageView2: UIImageView!
+final class ViewController: UIViewController {
+    private var leftDiceNumber = 1
+    private var rightDiceNumber = 5
+    
+    @IBOutlet weak private var diceImageView1: UIImageView!
+    @IBOutlet weak private var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        
+        leftDiceNumber += 1
+        rightDiceNumber -= 1
     }
 }
 
